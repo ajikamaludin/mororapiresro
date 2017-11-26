db7fe4380d0c856ffd6ae3845c2edc17e03cbfa2
Aji Kamaludin <aji19kamaludin@gmail.com>
2017-11-26 12:06:11 +0700

    menambahkan git ignore dan upload image melalui ajax

---
+++ image.ajax.php
@@ -0,0 +1,25 @@
+<?php
+
+include "init/init.php";
+
+if(isset($_FILES['file'])){
+
+    $file = $_FILES['file'];
+
+    $namaGambar = $file['name'];
+    $type = $file['type'];
+    $namaGambarFix = namaGambar($namaGambar,$type);
+    $pathGambar = $file['tmp_name'];
+    $simpan = "asset/image/menu/".$namaGambarFix;
+    move_uploaded_file($pathGambar, $simpan);
+
+    $error = $file['error'];
+    if($error == "0"){
+        echo "0";
+    }else{
+        echo $error;
+    }
+
+}else{
+    echo "gambar kosong";
+}
